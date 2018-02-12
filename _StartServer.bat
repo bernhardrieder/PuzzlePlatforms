dc47d223738ad255cb3f0996bd12497a96248262
Unknown <bernhard.rieder@live.at>
2018-02-11 23:07:59 -0500

    deleted some batch files

--- _StartServer.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\Epic Games\UE_4.18\Engine\Binaries\Win64\UE4Editor.exe" "D:\Game Development\Unreal Engine\MultiplayerMasteryCourse\PuzzlePlatforms\PuzzlePlatforms.uproject" /Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen -server -log
